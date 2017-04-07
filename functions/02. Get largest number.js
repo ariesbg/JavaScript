6beb63027ba8e1b036aa15c48cffba2db0b2ac87
Martina <martineli_martineli@abv.bg>
2017-04-07 14:59:39 +0200

    Add files via upload

---
+++ functions/02. Get largest number.js
@@ -0,0 +1,19 @@
+function getLargestNumber(args) {
+    var input = args[0].split(' ').map(Number),
+        firstNumber = input[0],
+        secondNumber = input[1],
+        thirdNumber = input[2];
+
+    if (getMax(firstNumber, secondNumber) > thirdNumber) {
+        return getMax(firstNumber, secondNumber);
+    } else {
+        return thirdNumber;
+    }
+
+    function getMax(first, second) {
+        return first > second ? first : second;
+    }
+}
+
+// test
+console.log(getLargestNumber(['7 19 18']));
