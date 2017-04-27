97bf8b9d821751bbc3d4bdb422a28cc73dccedd0
Martina <martineli_martineli@abv.bg>
2017-04-27 16:57:59 +0200

    Update 06. Group people.js

--- object/06. Group people.js
+++ object/06. Group people.js
@@ -1,4 +1,3 @@
-//masiv ot obekti
 var people = [
     {firstName: 'Marti', lastName: 'Naidenova', age: 25},
     {firstName: 'Iva', lastName: 'Petrova', age: 39},
@@ -7,16 +6,37 @@ var people = [
     {firstName: 'Mimo', lastName: 'Mimov', age: 25},
     {firstName: 'Tabak', lastName: 'Genadiev', age: 80}
 ];
+//First
+//console.log(people);
+//console.log("'25': ");
+//console.log(people.filter(x => x.age == 25));
+
+//console.log("'33': ");
+//console.log(people.filter(x => x.age == 33));
+
+//console.log("'39': ");
+//console.log(people.filter(x => x.age == 39));
+
+//console.log("'80': ");
+//console.log(people.filter(x => x.age == 80));
+
+//Second
 var sortedArr = people.sort((x, y) => x.age - y.age);
+
 console.log(sortedArr);
-console.log("'22': ");
-console.log(sortedArr.filter(x => x.age == 25));
 
-console.log("'33': ");
-console.log(sortedArr.filter(x => x.age ==33));
+var minAge = sortedArr[0].age;
+console.log('"' + minAge + '" :');
+console.log(people.filter(x => x.age == minAge));
 
-console.log("'39': ");
-console.log(sortedArr.filter(x => x.age ==39));
+var minAgePlus2 = sortedArr[2].age;
+console.log('"' + minAgePlus2 + '" :');
+console.log(people.filter(x => x.age == minAgePlus2));
 
-console.log("'80': ");
-console.log(sortedArr.filter(x => x.age ==80));
+var minAgePlus4 = sortedArr[4].age;
+console.log('"' + minAgePlus4 + '" :');
+console.log(people.filter(x => x.age == minAgePlus4));
+
+var minAgePlus5 = sortedArr[5].age;
+console.log('"' + minAgePlus5 + '" :');
+console.log(people.filter(x => x.age == minAgePlus5));
